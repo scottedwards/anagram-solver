5f71b8f4792b07b357b1dd9f077a82bbbfc71823
Scott Edwards <scottedwardsicoll@gmail.com>
2017-12-27 16:07:48 +0000

    Added a filter function to the class specification. removes elements from a set given a predicate.

--- AnagramDatabase.h
+++ AnagramDatabase.h
@@ -1,12 +1,12 @@
 #ifndef ANAGRAMDATABASE_HEADER
 #define ANAGRAMDATABASE_HEADER
 
-#include <vector>
+#include <set>
 #include <string>
 #include <unordered_map>
 
 //the type that the anagram database will have
-typedef std::unordered_map < int, std::vector < std::string > > char_sum_map;
+typedef std::unordered_map < int, std::set < std::string > > char_sum_map;
 typedef std::unordered_map < int, char_sum_map > anagram_db;
 
 /** 
@@ -36,7 +36,7 @@ class AnagramDatabase
          * Finds all possible anagrams of a word.
          * For this to work, the hashmaps must be populated.
          */
-        std::vector<std::string> findAnagrams(const std::string& anagram);
+        std::set<std::string> findAnagrams(const std::string& anagram);
         /**
          * Uses the given word file for a dictionary.
          */
@@ -46,11 +46,18 @@ class AnagramDatabase
          * Does this by comparing the amount of times a character pops up in
          * each word (they must be equal for all characters)
          */
-        bool isAnagram(const std::string& a1, const std::string& a2);
+        static bool isAnagram(const std::string& a1, const std::string& a2);
 
     private:
         anagram_db adb;
         int getWordSum(const std::string& word);
+        /**
+         * Given a set, a proposition and a predicate, it removes all elements
+         * that do not satisfy the predicate.
+         */
+        static void filter(std::set <std::string>& st, 
+            const std::string& proposition,
+            bool (*predicate)(const std::string& s1, const std::string& s2));
 };
 
 #endif
